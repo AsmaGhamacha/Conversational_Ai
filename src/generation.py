5610f0da390e4068a5940fffb1dca6d4b41c8400
AsmaGhamacha <97735282+AsmaGhamacha@users.noreply.github.com>
2025-05-10 21:55:50 +0200

    corrected the model name for the second time

--- src/generation.py
+++ src/generation.py
@@ -10,7 +10,7 @@ import os
 import requests
 
 class Generator:
-    def __init__(self, model_name="meta-llama/Meta-Llama-3-8B-Instruct"):
+    def __init__(self, model_name="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"):
         """Generator using Together.ai"""
         self.model = model_name
         self.api_key = st.secrets["TOGETHER_API_KEY"]
